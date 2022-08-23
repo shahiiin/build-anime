28094fdd02a31e9f8094330a055f3663628491fd
shahin <shahin.shahi1986@gmail.com>
2022-08-23 12:55:08 +0300

    make apicall

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react'
 
 function App() {
+  const getData = async () => {
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=naruto&limit=30`)
+
+    const resData = await res.json()
+    console.log(resData, 'xx')
+  }
+  useEffect(() => {
+    getData()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
