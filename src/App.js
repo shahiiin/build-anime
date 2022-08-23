7ca743c25595610255f3fde19d46b460a7651599
shahin <shahin.shahi1986@gmail.com>
2022-08-23 14:19:19 +0300

    make stayle of content

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import { useEffect, useState } from 'react'
+import { AnimeList } from './components/AnimeList'
 
 function App() {
   const [anime, setAnime] = useState('')
   const [search, setSearch] = useState('Naruto')
   const getData = async () => {
-    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=30`)
+    const res = await fetch(
+      `https://api.jikan.moe/v4/anime?q=${search}&limit=30`,
+    )
 
     const resData = await res.json()
     setAnime(resData.data)
   }
   useEffect(() => {
     getData()
-  }, [])
+  }, [search])
 
   return (
     <>
@@ -23,6 +26,15 @@ function App() {
             placeholder="search you are anime"
             onChange={(e) => setSearch(e.target.value)}
           />
+        </div>
+      </div>
+      <div className="container">
+        <div className="anime-info"></div>
+        <div className="anime-row">
+          <h2 className="text-heading">Anime</h2>
+          <div className="row">
+            <AnimeList />
+          </div>
         </div>
       </div>
     </>
