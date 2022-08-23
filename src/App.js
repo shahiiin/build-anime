753c6138d2faa9fe83f67c285dddf288bedd84fc
shahin <shahin.shahi1986@gmail.com>
2022-08-23 14:05:13 +0300

    start serch filter

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { useEffect, useState } from 'react'
 
 function App() {
   const [anime, setAnime] = useState('')
-
+  const [search, setSearch] = useState('Naruto')
   const getData = async () => {
-    const res = await fetch(`https://api.jikan.moe/v4/anime?q=naruto&limit=30`)
+    const res = await fetch(`https://api.jikan.moe/v4/anime?q=${search}&limit=30`)
 
     const resData = await res.json()
     setAnime(resData.data)
@@ -15,14 +15,17 @@ function App() {
 
   return (
     <>
-     <div className="header">
-      <h1>My Anime list</h1>
-      <div className='search-box'>
-        <input type='search' placeholder='search you are anime'/>
+      <div className="header">
+        <h1>My Anime list</h1>
+        <div className="search-box">
+          <input
+            type="search"
+            placeholder="search you are anime"
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </div>
       </div>
-    </div>
     </>
-   
   )
 }
 
